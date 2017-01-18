5936920a15e89a57b6421cf044499ef4e9127beb
Muhammad Iqbal Pamungkas <iqbalpmgks@gmail.com>
2017-01-18 14:12:28 +0700

    File master

---
+++ master/nav.php
@@ -0,0 +1,85 @@
+<div class="navbar-default sidebar" role="navigation">
+  <div class="sidebar-nav navbar-collapse">
+    <ul class="nav" id="side-menu">
+
+      <li>
+        <a href="../pages/index.php"><i class="glyphicon glyphicon-tasks"></i> DASHBOARD</a>
+      </li>
+      <li>
+        <a href="#"><i class="glyphicon glyphicon-inbox"></i> MASTER<span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li class='nav-item'>
+            <a class='nav-link' href='penyewa.php'><i class='fa fa-edit'></i> Entry Data Penyewa</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='mobil.php'><i class='fa fa-edit'></i> Entry Data Mobil</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='perlengkapan.php'><i class='fa fa-edit'></i> Entry Data Perlengkapan</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='supir.php'><i class='fa fa-edit'></i> Entry Data Supir</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='jns_denda.php'><i class='fa fa-edit'></i> Entry Data Jenis Denda</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='staff.php'><i class='fa fa-edit'></i> Entry Data Staff</a>
+          </li>
+        </ul>
+        <!-- /.nav-second-level -->
+      </li>
+      <li>
+        <a href="#"><i class="glyphicon glyphicon-inbox"></i> TRANSAKSI<span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li class='nav-item'>
+            <a class='nav-link' href='../transaksi/spsk.php'><i class='fa fa-edit'></i> Cetak SPSK</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../transaksi/invoice.php'><i class='fa fa-edit'></i> Cetak Invoice</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../transaksi/kwitansi.php'><i class='fa fa-edit'></i> Cetak Kwitansi</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../transaksi/spk.php'><i class='fa fa-edit'></i> Cetak SPK</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../transaksi/stk.php'><i class='fa fa-edit'></i> Cetak STK</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../transaksi/pengembalian.php'><i class='fa fa-edit'></i> Entri Pengembalian</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../transaksi/bukden.php'><i class='fa fa-edit'></i> Cetak Bukti Denda</a>
+          </li>
+        </ul>
+        <!-- /.nav-second-level -->
+      </li>
+      <li>
+        <a href="#"><i class="glyphicon glyphicon-inbox"></i> LAPORAN<span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li class='nav-item'>
+            <a class='nav-link' href='../laporan/lapsewa.php'><i class='fa fa-edit'></i> Cetak Lap. Penyewaan</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../laporan/lapkembali.php'><i class='fa fa-edit'></i> Cetak Lap. Pengembalian</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../laporan/lapdenda.php'><i class='fa fa-edit'></i> Cetak Lap. Denda</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../laporan/lapsupir.php'><i class='fa fa-edit'></i> Cetak Lap. Supir</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../laporan/laprekap.php'><i class='fa fa-edit'></i> Laporan Rekapitulasi Mobil</a>
+          </li>
+        </ul>
+        <!-- /.nav-second-level -->
+      </li>
+    </ul>
+  </div>
+  <!-- /.sidebar-collapse -->
+</div>
+<!-- /.navbar-static-side -->
+</nav>
